6d47b1b58abda0660322a5b2b53c36f3bc2608b2
Shuliman <Julik.schuldiner@proton.me>
2024-03-15 18:42:39 +0100

    init

---
+++ src/test/java/com/example/coursegame/CourseGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
