8e11d3b42f62372f5bc9af0b0798b7d12ccab9ef
Shuliman <Julik.schuldiner@proton.me>
2024-03-23 13:49:36 +0100

    Removed redundant driver logic

--- src/main/java/com/example/coursegame/decorator/Taxi.java
+++ src/main/java/com/example/coursegame/decorator/Taxi.java
@@ -1,11 +1,6 @@
 package com.example.coursegame.decorator;
 
 public interface Taxi {
-    void setWaiting();
-    void setInTransit();
-    void setArrived();
     String getTaxiType();
-//    String getDriverStatus();
-
     int getSpeed();
 }
--- src/main/java/com/example/coursegame/enitiy/BaseTaxi.java
+++ src/main/java/com/example/coursegame/enitiy/BaseTaxi.java
@@ -8,22 +8,6 @@ public abstract class BaseTaxi implements Taxi {
     private String driverStatus;
     private TaxiState taxiState;
     private int speed;
-
-    @Override
-    public void setWaiting() {
-        this.taxiState = TaxiState.WAITING;
-    }
-
-    @Override
-    public void setInTransit() {
-        this.taxiState = TaxiState.IN_TRANSIT;
-    }
-
-    @Override
-    public void setArrived() {
-        this.taxiState = TaxiState.ARRIVED;
-    }
     public  abstract int getSpeed();
-
     public abstract double getRideCost(double distance);
 }
