6fe582ab6e3016732aefe19939df85a04842de03
Shuliman <Julik.schuldiner@proton.me>
2024-03-23 14:08:16 +0100

    Removed unnecessary taxi state

--- src/main/java/com/example/coursegame/enitiy/BaseTaxi.java
+++ src/main/java/com/example/coursegame/enitiy/BaseTaxi.java
@@ -1,11 +1,9 @@
 package com.example.coursegame.enitiy;
 
-import com.example.coursegame.state.TaxiState;
 import com.example.coursegame.decorator.Taxi;
 
 public abstract class BaseTaxi implements Taxi {
     private String taxiType;
-    private String driverStatus;
     private int speed;
     public  abstract int getSpeed();
     public abstract double getRideCost(double distance);
